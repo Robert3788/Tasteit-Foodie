f2448326f5a6b9dc8d751410b94911e6c227742e
Robert3788 <robertprissmann@gmail.com>
2026-09-20 01:31:44 +0400

    Update [id].js

--- functions/recipe/[id].js
+++ functions/recipe/[id].js
@@ -333,9 +333,9 @@ export async function onRequestGet(context) {
       max-width:100vw;
       border-radius: 0;
     }
-    .panel-overlay { position:fixed; }
-    .toast { position:fixed; left:50%; transform: translateX(-50%); width:398px; max-width:calc(100vw - 32px); }
-  `;
+.panel-overlay { position:fixed; }
+.panel { left:50%; transform: translateX(-215px); width:430px; max-width:100vw; }
+.toast { position:fixed; left:50%; transform: translateX(-50%); width:398px; max-width:calc(100vw - 32px); }
 
   const html = `<!DOCTYPE html>
 <html lang="en">
